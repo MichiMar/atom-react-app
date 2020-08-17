8a6f7abf078919d2a9a76bc36916c86630beba63
MichiMar <michimar.alexis@gmail.com>
2020-08-17 16:59:55 -0500

    added todo cards title and description, fixed json file error

--- src/App.js
+++ src/App.js
@@ -5,14 +5,37 @@ import './App.css';
 import { todos } from './todos.json';
 
 class App extends Component {
+  constructor () {
+    super ();
+    this.state = {
+      todos
+    }
+  }
+
   render () {
+  const todos = this.state.todos.map((todo, i) => {
+    return (
+      <div className="card">
+        <div className="card-header">
+          <h3>{todo.title}</h3>
+        </div>
+        <div className="card-body">
+          <p>{todo.description}</p>
+        </div>
+      </div>
+    )
+  })
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
-        <a href="" class="text-white">
-          Tasks
+        <a href="" className="text-white">
+          Task
         </a>
       </nav>
+
+      { todos }
+
       <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
