ebafd295e4f44ee8aa44a121bfc16fb128bc64b2
MichiMar <michimar.alexis@gmail.com>
2020-08-17 21:06:03 -0500

    todos form

--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -1,3 +1,76 @@
 import React, { Component } from 'react';
 
-class TodoForm extends Component {}
+class TodoForm extends Component {
+  constructor () {
+    super();
+      this.state = {
+        title: '',
+        responsible: '',
+        description: '',
+        priority: 'low'
+      };
+      this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
+  handleInputChange(e) {
+    const {value, name} = e.target;
+    console.log(value, name);
+    this.setState({
+      [name]: value
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="card">
+        <form className="card-body">
+          <div className="form-group">
+            <input
+              type="text"
+              name="title"
+              className="form-control"
+              value={this.state.title}
+              onChange={this.handleInputChange}
+              placeholder="Title"
+              />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              name="title"
+              className="form-control"
+              value={this.state.title}
+              onChange={this.handleInputChange}
+              placeholder="Responsible"
+              />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              name="title"
+              className="form-control"
+              value={this.state.title}
+              onChange={this.handleInputChange}
+              placeholder="Description"
+              />
+          </div>
+          <div className="form-group">
+            <select
+              name="priority"
+              className="form-control"
+              value={this.state.priority}
+              onChange={this.handleInputChange}
+              >
+              <option>low</option>
+              <option>medium</option>
+              <option>high</option>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            save
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
